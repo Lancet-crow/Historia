0ffe1c000c8db2573c6d9dd7f1010ca74cdc31e6
Alex-Degtyarenko <aleksdegtyarenko20@gmail.com>
2024-04-13 21:43:12 +0400

    Исправил ошибку с авторизацией пользователя, полностью перенёс систему авторизации на всплывающие окна.

--- main.py
+++ main.py
@@ -21,19 +21,32 @@ def load_user(user_id):
 
 @app.route('/', methods=['GET', 'POST'])
 def main_page():
-    form = RegisterForm()
-    if form.validate_on_submit():
-        if form.password.data != form.password_again.data:
-            return render_template('main_page.html', title="Main Page", form=form, message="Пароли не совпадают")
+    reg_form = RegisterForm()
+    log_form = LoginForm()
+
+    if reg_form.validate_on_submit():
+        if reg_form.password.data != reg_form.password_again.data:
+            return render_template('main_page.html', title="Main Page", reg_form=reg_form, log_form=log_form,
+                                   message="Пароли не совпадают")
         db_sess = db_session.create_session()
-        if db_sess.query(User).filter(User.email == form.email.data).first():
-            return render_template('main_page.html', title="Main Page", form=form, message="Такой пользователь уже есть")
+        if db_sess.query(User).filter(User.email == reg_form.email.data).first():
+            return render_template('main_page.html', title="Main Page", reg_form=reg_form, log_form=log_form,
+                                   message="Такой пользователь уже есть")
         user = User()
-        user.name, user.email, user.about = form.name.data, form.email.data, form.about.data
-        user.set_password(form.password.data)
+        user.name, user.email, user.about = reg_form.name.data, reg_form.email.data, reg_form.about.data
+        user.set_password(reg_form.password.data)
         db_sess.add(user)
         db_sess.commit()
-    return render_template('main_page.html', title="Main Page", form=form)
+
+    if log_form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == log_form.email.data).first()
+        if user and user.check_password(log_form.password.data):
+            login_user(user, remember=log_form.remember_me.data)
+            return redirect("/")
+        return render_template('main_page.html', title="Main Page",
+                               message="Неправильный логин или пароль", reg_form=reg_form, log_form=log_form)
+    return render_template('main_page.html', title="Main Page", reg_form=reg_form, log_form=log_form)
 
 
 @app.route('/profile/<username>')
@@ -42,42 +55,6 @@ def profile(username):
     if username == current_user.name:
         return render_template('profile.html', user=current_user)
 
-
-# @app.route('/register', methods=['GET', 'POST'])
-# def reqister():
-#     form = RegisterForm()
-#     if form.validate_on_submit():
-#         if form.password.data != form.password_again.data:
-#             return render_template('register.html', title='Регистрация',
-#                                    form=form,
-#                                    message="Пароли не совпадают")
-#         db_sess = db_session.create_session()
-#         if db_sess.query(User).filter(User.email == form.email.data).first():
-#             return render_template('register.html', title='Регистрация',
-#                                    form=form,
-#                                    message="Такой пользователь уже есть")
-#         user = User()
-#         user.name, user.email, user.about = form.name.data, form.email.data, form.about.data
-#         user.set_password(form.password.data)
-#         db_sess.add(user)
-#         db_sess.commit()
-#         return redirect('/login')
-#     return render_template('register.html', title='Регистрация', form=form)
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        db_sess = db_session.create_session()
-        user = db_sess.query(User).filter(User.email == form.email.data).first()
-        if user and user.check_password(form.password.data):
-            login_user(user, remember=form.remember_me.data)
-            return redirect("/")
-        return render_template('login.html',
-                               message="Неправильный логин или пароль",
-                               form=form)
-    return render_template('login.html', title='Авторизация', form=form)
 
 
 @app.route('/logout')
